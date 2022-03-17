ab1d1c6fcaf0c0daa1b7864979d42cf7dca7be6a
Mukesh Solanki <me@mukeshsolanki.com>
2022-03-17 16:42:25 +0530

    Initial Version

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# bundletool
+# BundleTool Action
+
+This action will help you convert your aab to signed apk file.
+
+## Inputs
+
+### `aabFile`
+
+**Required:** The relative path in your project where your Android bundle file will be located
+
+### `base64Keystore`
+
+**Required:** The base64 encoded signing key used to sign your apk
+
+This action will directly decode this input to a file to sign your release with. You can prepare your key by running this command on linux systems.
+
+```bash
+openssl base64 < some_signing_key.jks | tr -d '\n' | tee some_signing_key.jks.base64.txt
+```
+Then copy the contents of the `.txt` file to your GH secrets
+
+### `keystoreAlias`
+
+**Required:** The alias of your signing key 
+
+### `keystorePassword`
+
+**Required:** The password to your signing keystore
+
+### `keyPassword`
+
+**Required:** The private key password for your signing keystore
+
+## Outputs
+Output variables are set both locally and in environment variables.
+
+### `apkPath`
+The path to the single release apk file that have been signed with this action.
+
+## Example usage
+
+### Single APK
+
+The output variable `signedReleaseFile` can be used in a release action.
+
+```yaml
+steps:
+  - name: Convert aab to apk
+    id: convert_aab
+    uses: mukeshsolanki/bundletool-action@v1.0.0
+    with:
+      aabFile: app/build/outputs/bundle/release/app-release.aab
+      base64Keystore: ${{ secrets.BASE64_KEY }}
+      keystorePassword: ${{ secrets.PASSWORD }}
+      keystoreAlias: ${{ secrets.ALIAS }}
+      keyPassword: ${{ secrets.PASSWORD }}
+
+  - uses: actions/upload-artifact@v3
+    with:
+      name: release-apk
+      path: ${{ steps.convert_aab.outputs.apkPath }}
+```
---
+++ src/action.js
@@ -0,0 +1,89 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const tc = require('@actions/tool-cache');
+const io = require('@actions/io');
+const fs = require("fs");
+const path = require("path");
+
+const BUNDLETOOL_URL = "https://github.com/google/bundletool/releases/download/1.9.0/bundletool-all-1.9.0.jar"
+
+async function run() {
+    try {
+        if (process.platform !== "darwin" && process.platform !== "linux") {
+            throw new Error(
+                "Unsupported virtual machine: please use either macos or ubuntu VM."
+            );
+        }
+        // parameters passed to the plugin
+        const AAB_FILE = core.getInput('aabFile');
+        const BASE64_KEYSTORE = core.getInput('base64Keystore');
+        const KEYSTORE_PASSWORD = core.getInput('keystorePassword');
+        const KEYSTORE_ALIAS = core.getInput('keystoreAlias');
+        const KEY_PASSWORD = core.getInput('keyPassword');
+
+        // custom working directory
+        const workingDirectoryInput = core.getInput("working-directory");
+        if (workingDirectoryInput) {
+            console.log(`custom working directory: ${workingDirectoryInput}`);
+        }
+        const workingDirectory = !workingDirectoryInput ?
+            undefined :
+            workingDirectoryInput;
+
+        // execute the custom script
+        try {
+            // move to custom working directory if set
+            if (workingDirectory) {
+                process.chdir(workingDirectory);
+            }
+        } catch (error) {
+            core.setFailed(error.message);
+        }
+
+        const bundleToolPath = `${process.env.HOME}/bundletool`;
+        const bundleToolFile = `${bundleToolPath}/bundletool.jar`;
+
+        await io.mkdirP(bundleToolPath);
+
+        core.info(`${bundleToolPath} directory created`);
+
+        const downloadPath = await tc.downloadTool(BUNDLETOOL_URL);
+
+        await io.mv(downloadPath, bundleToolFile);
+
+        core.info(`${bundleToolFile} moved to directory`);
+
+        core.addPath(bundleToolPath);
+
+        core.info(`${bundleToolPath} added to path`);
+
+        await exec.exec(`chmod +x ${bundleToolFile}`);
+
+        await io.which("bundletool.jar", true);
+		
+        const signingKey = "signingKey.jks";
+
+        fs.writeFileSync(signingKey, BASE64_KEYSTORE, 'base64', function(err){
+        	if(err){
+	        	core.info(`Please check the key ${err}`);
+        	}else{
+	        	core.info(`KeyStore File Created`);        	
+        	}
+        });
+
+        var extension = path.extname(AAB_FILE);
+        var filename = path.basename(AAB_FILE, extension);
+        
+        await exec.exec(`java -jar ${bundleToolFile} build-apks --bundle=${AAB_FILE} --output=${filename}.apks --ks=${signingKey} --ks-pass=pass:${KEYSTORE_PASSWORD} --ks-key-alias=${KEYSTORE_ALIAS} --key-pass=pass:${KEY_PASSWORD} --mode=universal`);
+        await exec.exec(`mv ${filename}.apks ${filename}.zip`);
+        await exec.exec(`unzip ${filename}.zip`);
+        await exec.exec(`mv universal.apk ${filename}.apk`);
+        core.setOutput('apkPath', `${filename}.apk`);
+
+        await exec.exec(`rm -rf ${signingKey}`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run()
